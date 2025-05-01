c055cb21b972d533ad92c86ae7f5b761e3189a13
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2025-05-01 21:04:44 +0330

    --wip-- [skip ci]

--- config/type.go
+++ config/type.go
@@ -1,1 +1,26 @@
 package config
+
+type DBConfig struct {
+	Host     string `josn:"host"`
+	Port     int    `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	DbName   string `json:"db_name"`
+}
+
+type ServerConfig struct {
+	HttpPort          int    `json:"httpPort"`
+	Secret            string `json:"secret"`
+	AuthExpMin        int    `json:"authExpMin"`
+	AuthRefreshExpMin int    `json:"authRefreshExpMin"`
+}
+
+type RedisConfig struct {
+	Host string `json:"host"`
+	Port int    `json:"port"`
+}
+type Config struct {
+	DBConfig     DBConfig     `json:"dbConfig"`
+	ServerConfig ServerConfig `json:"serverConfig"`
+	RedisConfig  RedisConfig  `json:"redisConfig"`
+}
